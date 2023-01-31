6c1daa3e838337b6d577e36081e51cee13818de2
Emmanuel Donkor <edonkor0000@gmail.com>
2023-01-31 12:10:34 +0100

    Reverse string leetcode 344

---
+++ ReverseString/reverseString.js
@@ -0,0 +1,18 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  //using Two Pointers
+  //Initialze the pointer
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    let temp = s[left]; //store initial value the left in temp
+    s[left] = s[right]; //set the right to left
+    s[right] = temp; //set right to temp
+
+    left++;
+    right--;
+  }
+};
