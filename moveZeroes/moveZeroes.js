923476dc5963d5b1d8b47924ccaa67c3501c23a5
Emmanuel Donkor <edonkor0000@gmail.com>
2023-02-01 21:30:52 +0100

    move zeroes leetcode 283

---
+++ moveZeroes/moveZeroes.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  // Initialize the pointer that will keep track of the position where we should insert the next non-zero element
+  let insertPos = 0;
+
+  // Iterate through the array and move the non-zero elements to the front of the array
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[insertPos] = nums[i];
+      insertPos += 1;
+    }
+  }
+
+  // Insert the zeros at the end of the array
+  for (let i = insertPos; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+
+  return nums;
+};
